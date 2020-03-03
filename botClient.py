6ecc7aa4afe01930fc03b5351c386226132d8610
Sean Ashton <sean@ashtech.one>
2020-03-03 22:10:23 +0800

    Fixing timeout

--- botClient.py
+++ botClient.py
@@ -78,7 +78,7 @@ class BotClient( discord.Client ):
         votes_for = []
         while timeout > 0:
             votes_for_l = []
-            sleep_time = (time.time()-start_time) - (VOTE_TIME-timeout)
+            sleep_time = (VOTE_TIME-timeout) - (time.time()-start_time)
             print(str(time.time()-start_time),str(timeout),str(sleep_time))
             await asyncio.sleep(sleep_time)
             msg = discord.utils.get(await msg.channel.history().flatten(), id=msg.id)
