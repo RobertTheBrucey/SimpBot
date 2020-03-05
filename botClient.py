5ae498c17fa0cd87679b5a6f7dcdd456c8ec1ac2
Sean Ashton <sean@ashtech.one>
2020-03-05 21:26:24 +0800

    Fixed another thing

--- botClient.py
+++ botClient.py
@@ -84,7 +84,7 @@ class BotClient( discord.Client ):
         online_members = yes_count = no_count = 0
         votes_for_l = []
         votes_against_l = []
-        while timeout > 0:
+        while timeout > 0 and yes_count-no_count < online_members/2:
             timeout -= 5
             votes_for_l = []
             votes_against_l = []
